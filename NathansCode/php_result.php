ed1daacb1c930c081e2456681d41fb2f16fff185
nicole-ec <nicole.eneco@yahoo.ca>
2018-12-05 22:23:47 -0700

    updated login code

--- NathansCode/php_result.php
+++ NathansCode/php_result.php
@@ -1,14 +1,70 @@
+
 <?php
-    //echo "Start";
+
+require_once "dataBase_connect.php";
+
+if(isset($_POST['username'])){
+	
+	$username = stripslashes($_POST['username']);
+	$password = $_POST['password'];
+	
+	
+	//searches patient table
+	$sql = "SELECT patient_Fname, patient_Lname FROM PATIENTS WHERE patient_username = '".$username."' AND patient_password = '".$password."'";
+	
+	$result = mysqli_query($link, $sql);
+	
+	if (!mysqli_query($link,$sql)){
+	  die('Error: ' . mysqli_error($link));
+	}
+	
+	if(mysqli_num_rows($result) == 1){
+		
+		$pName = mysqli_fetch_assoc($result);
+		echo "Welcome ".$pName['patient_Fname']." ".$pName['patient_Lname']."!";
+		exit();
+	}
+	
+	//searches medstaff table
+	else{
+		
+		
+		$sql = "SELECT * FROM MEDICALSTAFF WHERE MedStaff_username = '".$username."' AND MedStaff_password = '".$password."'";
+		
+		$result = mysqli_query($link, $sql);
+	
+		if (!mysqli_query($link,$sql)){
+		  die('Error: ' . mysqli_error($link));
+		}
+		
+		if(mysqli_num_rows($result) == 1){
+			
+			$pName = mysqli_fetch_assoc($result);
+			echo "Welcome ".$pName['MedStaff_Fname']." ".$pName['MedStaff_Lname']."!";
+			exit();
+		}
+		else{
+			echo "login unsuccessful";
+			exit();
+		}
+		
+	}
+	
+}
+
     // Initialize the session
-    //session_start();
-    //echo "after session_start()";
+/*    
+	session_start();
+    
     // Check if the user is already logged in, if yes then redirect him to welcome page
-    //if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
         //header("location: welcome.php");
-        //exit;
-    //}
-    //echo "After isset loggedin check";
+        
+        exit;
+    }
+    
+*/    
+/*
     // Include config file
     require_once "dataBase_connect.php";
     
@@ -16,10 +72,10 @@
     $username = $password = "";
     $username_err = $password_err = "";
     
-    //echo "Testing before POST if statement";
+    
     // Processing form data when form is submitted
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-        //echo "Testing in POST if statement";
+        
         // Check if username is empty
         if(empty(trim($_POST["username"]))){
             $username_err = "Please enter username.";
@@ -36,10 +92,14 @@
         
         // Validate credentials
         if(empty($username_err) && empty($password_err)){
+			
             // Prepare a select statement
-            $sql = "SELECT patient_SIN, patient_username, patient_password FROM patients WHERE patient_username = ?";
+            $sql = "SELECT patient_username, patient_password FROM patients WHERE patient_username = '".$username."' and patient_password = '".$password."'";
+            
+            echo "made sql statement\n";
             
             if($stmt = mysqli_prepare($link, $sql)){
+				
                 // Bind variables to the prepared statement as parameters
                 mysqli_stmt_bind_param($stmt, "s", $param_username);
                 
@@ -48,6 +108,7 @@
                 
                 // Attempt to execute the prepared statement
                 if(mysqli_stmt_execute($stmt)){
+                    
                     // Store result
                     mysqli_stmt_store_result($stmt);
                     
@@ -58,12 +119,12 @@
                         if(mysqli_stmt_fetch($stmt)){
                             
                                 // Password is correct, so start a new session
-                                //session_start();
+//                                session_start();
                                 
                                 // Store data in session variables
-                                //$_SESSION["loggedin"] = true;
-                                //$_SESSION["id"] = $id;
-                                //$_SESSION["username"] = $username;
+                                $_SESSION["loggedin"] = true;
+                                $_SESSION["id"] = $id;
+                                $_SESSION["username"] = $username;
                                 
                                 // Redirect user to welcome page
                                 header("location: test.html");
@@ -78,10 +139,15 @@
             }
             
             // Close statement
+            if($stmt == false){
+				header("");
+				die("Couldn't connect ".mysqli_connect_error());
+			}
             mysqli_stmt_close($stmt);
         }
         
         // Close connection
         mysqli_close($link);
     }
+    */
     ?>
